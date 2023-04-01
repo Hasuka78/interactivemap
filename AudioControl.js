72a9a4c060d43936f9c40a7cf239d680d072933b
Hasuka78 <129599587+Hasuka78@users.noreply.github.com>
2023-04-01 15:30:19 +0200

    sound issue fix test n°idk

--- AudioControl.js
+++ AudioControl.js
@@ -1,14 +1,21 @@
 const audio = document.getElementById("background-audio");
+const playBtn = document.getElementById("play-btn");
 const muteUnmuteBtn = document.getElementById("mute-unmute-btn");
 const volumeSlider = document.getElementById("volume-slider");
 
+// Event listener for play button
+playBtn.addEventListener("click", () => {
+  audio.play();
+  playBtn.style.display = "none";
+});
+
 // Event listener for mute-unmute button
 muteUnmuteBtn.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.play();
+  if (audio.muted) {
+    audio.muted = false;
     muteUnmuteBtn.innerText = "Mute";
   } else {
-    audio.pause();
+    audio.muted = true;
     muteUnmuteBtn.innerText = "Unmute";
   }
 });
